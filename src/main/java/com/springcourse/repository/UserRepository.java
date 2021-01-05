a86d8efa77b17276f9bd6b7dad1a736aadc92d22
Jhonatas Henrique Barbosa do Carmo <jhonatashbc@hotmail.com>
2021-01-05 13:54:56 -0300

    Criação do repositório da entidade user

---
+++ src/main/java/com/springcourse/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.springcourse.repository;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import com.springcourse.domain.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long>{
+
+	public List<User> findByName(String name);
+	
+	public Optional<User> findByEmail(String email);
+	
+	public Optional<User> findByEmailAndPassword(String email, String password);
+	
+	@Query("SELECT FROM User WHERE email = ?1 AND password = ?2")
+	public Optional<User> login(String email, String password);
+}
