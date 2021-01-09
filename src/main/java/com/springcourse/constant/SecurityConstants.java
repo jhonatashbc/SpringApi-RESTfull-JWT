c249bad3d35ec022edf55bbf4007289c23a010cd
Jhonatas Henrique Barbosa do Carmo <jhonatashbc@hotmail.com>
2021-01-09 16:28:29 -0300

    Criação das constantes de segurança

---
+++ src/main/java/com/springcourse/constant/SecurityConstants.java
@@ -0,0 +1,9 @@
+package com.springcourse.constant;
+
+public class SecurityConstants {
+
+	public static final int JTW_EX_DAYS = 5;
+	public static final String API_KEY = "spring-api-restful";
+	public static final String JWT_PROVIDER = "Bearer";
+	public static final String JWT_ROLE_KEY = "role";
+}
