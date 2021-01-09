a95d1ab29be3cb1b182c86ba7a89320965a33a0a
Jhonatas Henrique Barbosa do Carmo <jhonatashbc@hotmail.com>
2021-01-09 11:41:25 -0300

    Revert "Revert "Criação das validações para a entidade Request""
    This reverts commit 8e76f6fbc147ac9da926d12fd6b7cf6de269893e.

---
+++ src/main/java/com/springcourse/dto/RequestSavedto.java
@@ -0,0 +1,38 @@
+package com.springcourse.dto;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+import com.springcourse.domain.Request;
+import com.springcourse.domain.RequestStage;
+import com.springcourse.domain.User;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RequestSavedto {
+
+	@NotBlank(message = "Subject required")
+	private String subject;
+
+	private String description;
+
+	@NotNull(message = "Owner required")
+	private User owner;
+
+	private List<RequestStage> stages = new ArrayList<>();
+
+	public Request transformToRequest() {
+		Request request = new Request(null, this.subject, this.description, null, null, this.owner, this.stages);
+		return request;
+	}
+}
