36dcd703b2d9d4acaa6219926d3ada82d131a56d
Jhonatas Henrique Barbosa do Carmo <jhonatashbc@hotmail.com>
2021-01-09 17:46:07 -0300

    Adicionado a constante de mensagem de token inválido

--- src/main/java/com/springcourse/constant/SecurityConstants.java
+++ src/main/java/com/springcourse/constant/SecurityConstants.java
@@ -6,4 +6,5 @@ public class SecurityConstants {
 	public static final String API_KEY = "spring-api-restful";
 	public static final String JWT_PROVIDER = "Bearer";
 	public static final String JWT_ROLE_KEY = "role";
+	public static final String JWT_INVALID_MSG = "Invalid token";
 }
