f77bef6558aa5727ded250de5adc591d41e73fba
Jhonatas Henrique Barbosa do Carmo <jhonatashbc@hotmail.com>
2021-01-09 14:54:09 -0300

    Exposição do AuthenticationManager como Bean

--- src/main/java/com/springcourse/security/SecurityConfig.java
+++ src/main/java/com/springcourse/security/SecurityConfig.java
@@ -1,12 +1,35 @@
 package com.springcourse.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.BeanIds;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+import com.springcourse.service.UserService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
 
+	@Autowired
+	private UserService userService;
 	
+	@Autowired
+	private CustomPasswordEncoder passwordEncoder;
+	
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userService).passwordEncoder(passwordEncoder);
+	}
+	
+	@Bean(name = BeanIds.AUTHENTICATION_MANAGER)
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		
+		return super.authenticationManagerBean();
+	}
 }
