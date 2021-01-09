98180010f07c29b60401dd7d2c7f5a1dcbd78b22
Jhonatas Henrique Barbosa do Carmo <jhonatashbc@hotmail.com>
2021-01-09 16:52:14 -0300

    Correção do nome da classe e método e configuração de um handler para a exceção Bad Credentials

--- src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
+++ src/main/java/com/springcourse/resource/exception/ResourceExceptionHandler.java
@@ -7,6 +7,7 @@ import java.util.List;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -16,13 +17,20 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 import com.springcourse.exception.NotFoundException;
 
 @ControllerAdvice
-public class ResourceExpectionHandler extends ResponseEntityExceptionHandler {
+public class ResourceExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(NotFoundException.class)
-	public ResponseEntity<ApiError> handlerNotFoundExpection(NotFoundException ex) {
+	public ResponseEntity<ApiError> handlerNotFoundException(NotFoundException ex) {
 		ApiError error = new ApiError(HttpStatus.NOT_FOUND.value(), ex.getMessage(), new Date());
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+	
+	@ExceptionHandler(BadCredentialsException.class)
+	public ResponseEntity<ApiError> handlerBadCredentialsException(BadCredentialsException ex) {
+		ApiError error = new ApiError(HttpStatus.UNAUTHORIZED.value(), ex.getMessage(), new Date());
+
+		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(error);
 	}
 
 	@Override
