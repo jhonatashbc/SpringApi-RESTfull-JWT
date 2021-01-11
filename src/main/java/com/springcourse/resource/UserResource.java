88b05c29af2c57843af4a14d169f6a4f8052a6c6
Jhonatas Henrique Barbosa do Carmo <jhonatashbc@hotmail.com>
2021-01-11 10:02:18 -0300

    Adicionado controle de permissão para os métodos necessários

--- src/main/java/com/springcourse/resource/UserResource.java
+++ src/main/java/com/springcourse/resource/UserResource.java
@@ -8,6 +8,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -51,6 +52,7 @@ public class UserResource {
 	@Autowired
 	private JwtManager jwtManager;
 
+	@Secured({"ROLE_ADMINISTRATOR"})
 	@PostMapping
 	public ResponseEntity<User> save(@RequestBody @Valid UserSavedto userdto) {
 		User createdUser = userService.save(userdto.transformToUser());
@@ -107,6 +109,7 @@ public class UserResource {
 		return ResponseEntity.ok(pm);
 	}
 
+	@Secured({"ROLE_ADMINISTRATOR"})
 	@PatchMapping("role/{id}")
 	public ResponseEntity<?> updateRole(@RequestBody @Valid UserUpdateRoledto userRole,
 			@PathVariable(name = "id") Long id) {
