60c0285f78b6f2e922e03033ee2ee0f57b3fbff0
Jhonatas Henrique Barbosa do Carmo <jhonatashbc@hotmail.com>
2021-01-16 11:05:30 -0300

    Configurando JpaSpecification no repositório da entidade Client

--- src/main/java/com/springcourse/repository/ClientRepository.java
+++ src/main/java/com/springcourse/repository/ClientRepository.java
@@ -2,12 +2,13 @@ package com.springcourse.repository;
 
 import com.springcourse.domain.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 
 @Repository
-public interface ClientRepository extends JpaRepository<Client, Long> {
+public interface ClientRepository extends JpaRepository<Client, Long>, JpaSpecificationExecutor<Client> {
 
     Optional<Client> findByIdentificationNumber(String identificationNumber);
 }
